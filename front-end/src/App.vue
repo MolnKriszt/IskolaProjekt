95c8644c9c62a00757f1b429383edfa22833ad18
MolnKriszt <krisz200601@gmail.com>
2024-12-10 13:06:18 +0100

    start

---
+++ front-end/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+<script>
+export default {
+  data(){
+    return{
+      url: 'http://localhost:8000/api'
+    }
+  }
+}
+</script>
+
+<template>
+  <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+  </div>
+</nav>
+  <header>
+
+    <div class="">
+      <nav>
+        <RouterLink to="/">Home</RouterLink> | 
+        <RouterLink to="/kartyak">Kártyák</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView
+    :url="url"
+  />
+</template>
+
+<style scoped>
+
+</style>
